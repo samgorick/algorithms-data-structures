a334a4b9ddf815687b8410a2fee88d51186a4def
gozmeister <samgorick.sg@gmail.com>
2020-08-31 13:54:43 -0500

    tree intro

---
+++ udemy-course/data-structures/tree.rb
@@ -0,0 +1,19 @@
+# Trees
+# Each node can reference +/- 1 node
+# Must have one entry point (ROOT) only
+# Sibling - a group of nodes with the same parent
+# Leaf - a node with no children
+# Edge - Connection between parent and child
+
+# Use cases:
+# HTML DOM
+# Network Routing
+# Hierarchical folder structure on PC
+# JSON
+
+# Binary Search Trees
+# Each parent can have max. 2 children
+# Special property: Every item on the left of a node is lesser
+# Every item on the right of a node is greater
+# Excel at sorting/accessing data that can be sortable
+
